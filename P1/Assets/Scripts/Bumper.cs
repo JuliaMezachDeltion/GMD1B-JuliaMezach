353cb068c627779f24e724ce8b78dc33a1e67141
JuliaMezachDeltion <juliamezach.jm@gmail.com>
2017-01-27 14:42:48 +0100

    14:41 27-1-2017

--- P1/Assets/Scripts/Bumper.cs
+++ P1/Assets/Scripts/Bumper.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class Bumper : MonoBehaviour {
 
-    public Rigidbody bal;
-
     void OnCollisionEnter(Collision Collision)
     {
-        GameObject.Find("GameManager").GetComponent<GameManager>().score += 10;
+        GameObject.Find("Sphere").GetComponent<Test>().Testvoid(score);
     }
 }
--- P1/Assets/Scripts/Launcher.cs
+++ P1/Assets/Scripts/Launcher.cs
@@ -20,7 +20,6 @@ public class Launcher : MonoBehaviour
                     SpawnPinball();
                 }
             }
-            activeball 
         }
     }
 
